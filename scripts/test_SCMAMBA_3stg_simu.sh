f34a3e394e69af35d472da57c9e20098c9e6e22b
FengYuchao <35914095+fengyuchao97@users.noreply.github.com>
2025-09-23 15:56:46 +0800

    Add files via upload

---
+++ scripts/test_SCMAMBA_3stg_simu.sh
@@ -0,0 +1,2 @@
+python tools/test_simu.py \
+    --config-file configs/dernn_lnlt_3stg_simu.yaml 
---
+++ scripts/train_SCMAMBA_3stg_real.sh
@@ -0,0 +1,2 @@
+python tools/train_our_acc_pall.py \
+    --config-file configs/dernn_lnlt_3stg_real.yaml
